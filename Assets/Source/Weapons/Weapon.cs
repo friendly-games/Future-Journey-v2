b4852b777a9447a2882116a62696b44a10e04e3b
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2017-07-03 13:08:20 -0500

    Initial commit

---
+++ Assets/Source/Weapons/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NineBitByte.Assets.Source
+{
+  [CreateAssetMenu(menuName = "Weapon")]
+  public class Weapon : BaseScriptable
+  {
+    public string Name;
+    public int DamagePerShot = 10;
+
+    [Range(1, 20)]
+    public int NumberOfPelots = 1;
+
+    [Range(0, 1)]
+    public float Spread = .75f;
+
+    [Range(1, 150)]
+    public int ClipSize = 1;
+
+    public GameObject WeaponTemplate;
+    public GameObject AmmoTemplate;
+  }
+}
---
+++ Assets/Source/WorldManagerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NineBitByte.Assets.Scripts
+{
+  public class WorldManagerBehavior : MonoBehaviour
+  {
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+    }
+  }
+}
