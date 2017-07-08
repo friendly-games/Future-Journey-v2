bfedd531601e2a555556a4b1763399d6799598a8
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2017-07-08 18:20:02 -0500

    Add clip size to weapons + requirement to reload weapons

--- Assets/Source/FutureJourney/Items/PlayerBehavior.cs
+++ Assets/Source/FutureJourney/Items/PlayerBehavior.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using NineBitByte.Common;
 using NineBitByte.FutureJourney.Programming;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NineBitByte.FutureJourney.Items
 {
@@ -18,15 +19,20 @@ namespace NineBitByte.FutureJourney.Items
     [Tooltip("The location to which the weapon should be placed on the player")]
     public RelativeOffset WeaponOffset;
 
-    private ProjectileWeapon _currentWeapon;
+    [Tooltip("Location to write information about the current weapon/ammo")]
+    public Text EquipmentInformation;
 
     private Ownership<ProjectileWeapon, WeaponBehavior> _selectedWeapon;
 
     private Rigidbody2D _rigidBody;
+
     private Transform _reticule;
+
     private Transform _overallBody;
 
     private PlayerInputHandler _playerInputHandler;
+
+    private int _numberOfRemainingShots;
 
     public void Start()
     {
@@ -45,17 +51,34 @@ namespace NineBitByte.FutureJourney.Items
       SelectWeapon(AvailableWeapons.FirstOrDefault());
     }
 
+    /// <summary> The total number of remaining bullets we have to fire. </summary>
+    private int NumberOfRemainingShots
+    {
+      get { return _numberOfRemainingShots; }
+      set
+      {
+        _numberOfRemainingShots = value;
+        EquipmentInformation.text = $"{NumberOfRemainingShots}/{_selectedWeapon.Programming.ClipSize}";
+      }
+    }
+
     private void SelectWeapon(ProjectileWeapon weapon)
     {
       _selectedWeapon.Destroy();
       weapon.Attach(ref _selectedWeapon, WeaponOffset.ToLocation(transform));
+      Reload();
+    }
+
+    public void Reload()
+    {
+      NumberOfRemainingShots = _selectedWeapon.Programming.ClipSize;
     }
 
     public void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space))
+      if (Input.GetMouseButtonDown(0))
       {
-        _selectedWeapon.Programming?.Act(_selectedWeapon.Behavior, Allegiance);
+        ActWithCurrentlyEquippedItem();
       }
 
       if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -67,7 +90,21 @@ namespace NineBitByte.FutureJourney.Items
         SelectWeapon(AvailableWeapons[1]);
       }
 
+      if (Input.GetKeyDown(KeyCode.R))
+      {
+        Reload();
+      }
+
       _playerInputHandler.Recalculate();
+    }
+
+    private void ActWithCurrentlyEquippedItem()
+    {
+      if (NumberOfRemainingShots <= 0)
+        return;
+
+      _selectedWeapon.Programming?.Act(_selectedWeapon.Behavior, Allegiance);
+      NumberOfRemainingShots--;
     }
 
     public void FixedUpdate()
